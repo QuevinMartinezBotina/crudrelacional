5e267e33c28f325bbf67590de84c7a4ef6ea9240
quevinmartinezbotina <quevinmartinezbotina@gmail.com>
2022-02-03 14:05:42 -0500

    migraciones y relacion creadas

---
+++ database/migrations/2022_02_03_184646_create_libros_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->engine = "InnoDB";
+            $table->id();
+
+            $table->bigInteger("categoria_id")->unsigned();
+
+            $table->timestamps();
+
+            $table->foreign("categoria_id")->references('id')->on("categorias")
+                ->onDelete("cascade")
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
